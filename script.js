5274d45aed20515e7925e1dca85d3292835d4246
Ramon-Carrillo <carrillojrramon@gmail.com>
2021-10-25 22:47:07 -0500

    added a function to test form validation without HTML.

--- script.js
+++ script.js
@@ -2,4 +2,25 @@ const form = document.getElementById('form');
 const password1El = document.getElementById('password1');
 const password2El = document.getElementById('password2');
 const messageContainer = document.querySelector('.message-container');
-const message = document.getElementId('message');
+const message = document.getElementById('message');
+
+let isValid = false;
+
+const validateForm = () => {
+  // Using constraint API
+  isValid = form.checkValidity();
+  //Style main message for an error
+  message.textContent = 'Please fill out all the required fields.';
+  message.style.color = 'red';
+  messageContainer.style.borderColor = 'red';
+};
+
+const processFormData = (e) => {
+  e.preventDefault();
+
+  //validate form
+  validateForm();
+};
+
+//Event Listener
+form.addEventListener('submit', processFormData);
